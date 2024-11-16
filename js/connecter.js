70c72f85a137fcc06143252c3d4645c3007a2875
marc14444 <amanijeanmarc41@gmail.com>
2024-11-16 23:31:56 +0000

    lllmlmmmmm

--- js/connecter.js
+++ js/connecter.js
@@ -1,23 +1,26 @@
-const user = JSON.parse(localStorage.getItem('userData'));
+document.addEventListener('DOMContentLoaded', () => {
+    const user = JSON.parse(localStorage.getItem('userData'));
 
-if (user) {
-    document.getElementById('userEmail').textContent = `Bonjour🖖, ${user.data.nom}`;
-} else {
-    window.location.href = '../index.html';
-}
-
-
-function isAuthenticated() {
-    const token = localStorage.getItem('adminToken');
-    if (token) {
-        // Vérifier si le token est valide (vous pouvez ajouter une logique supplémentaire ici)
-        return true;
+    if (user) {
+        document.getElementById('userEmail').textContent = `Bonjour🖖, ${user.data.nom}`;
+    } else {
+        window.location.href = '../index.html';
     }
-    return false;
-}
-// Exemple d'utilisation de la fonction de vérification de session
-if (isAuthenticated()) {
-    console.log("L'admin est authentifié");
-} else {
-    console.log("L'admin n'est pas authentifié");
-}
+    
+    
+    function isAuthenticated() {
+        const token = localStorage.getItem('adminToken');
+        if (token) {
+            // Vérifier si le token est valide (vous pouvez ajouter une logique supplémentaire ici)
+            return true;
+        }
+        return false;
+    }
+    // Exemple d'utilisation de la fonction de vérification de session
+    if (isAuthenticated()) {
+        console.log("L'admin est authentifié");
+    } else {
+        console.log("L'admin n'est pas authentifié");
+    }
+     
+});
