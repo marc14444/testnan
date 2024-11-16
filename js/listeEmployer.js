df03a072b730ecf76d45e7b87df657384bc334b9
marc14444 <amanijeanmarc41@gmail.com>
2024-11-16 22:30:46 +0000

    add email for admin

--- js/listeEmployer.js
+++ js/listeEmployer.js
@@ -293,21 +293,3 @@ function updateEmployeeInUI(employe) {
   }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    const token = localStorage.getItem('adminToken');
-    const emailElement = document.getElementById('userEmail');
-
-    if (token) {
-        // Décoder le token (attention, ce n'est pas sécurisé côté client pour des actions sensibles)
-        const payload = JSON.parse(atob(token.split('.')[1])); // Décodage base64
-        const email = payload.email;
-
-        if (email) {
-            emailElement.textContent = email;
-        } else {
-            emailElement.textContent = 'Email non disponible';
-        }
-    } else {
-        emailElement.textContent = 'Non connecté';
-    }
-});
